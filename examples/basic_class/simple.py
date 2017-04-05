631dceb861b70388a7f5d9aabc9680bdef6f178a
petspats <gkarm@live.com>
2017-04-05 19:44:07 +0300

    restore mac algo

--- examples/basic_class/simple.py
+++ examples/basic_class/simple.py
@@ -2,27 +2,28 @@ from pyha.common.const import Const
 from pyha.common.sfix import Sfix, resize, fixed_truncate
 from pyha.common.hwsim import HW
 import numpy as np
-from pyha.simulation.simulation_interface import debug_assert_sim_match, SIM_GATE
+from pyha.simulation.simulation_interface import debug_assert_sim_match, SIM_GATE, plot_assert_sim_match
 
 
 class Simple(HW):
     def __init__(self):
-        self.coef = 0
+        self.coef = 123
         self.mul = 0
-        self.sum = 0
+        self.acc = 0
 
-    def main(self, a, b):
-        self.next.mul = a * self.coef
-        self.next.sum = self.mul + b
-        return self.sum
+    def main(self, a):
+
+        self.next.mul = self.coef * a
+        self.next.acc = self.acc + self.mul
+        return self.acc
 
 
 def test_basic():
     from pyha.simulation.simulation_interface import assert_sim_match, SIM_HW_MODEL, SIM_RTL
     dut = Simple()
-    inputs = [[1, 2], [3,4]]
+    inputs = [1, 2, 3, 4, 5, 6, 7, 8]
 
-    assert_sim_match(dut, None, *inputs,
+    plot_assert_sim_match(dut, None, inputs,
                      simulations=[SIM_HW_MODEL, SIM_RTL, SIM_GATE],
                      rtol=1e-4,
                      dir_path='/home/gaspar/git/thesis/playground')
