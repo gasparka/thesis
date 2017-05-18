6baa212210daab005b2bbf32ea416ae53f09e0fd
petspats <gkarm@live.com>
2017-05-18 21:46:02 +0300

    last version, to print

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ author = 'Gaspar Karm'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1.0'
+version = '1.2.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
